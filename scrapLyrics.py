20138b1f31af914aa15790e85ea4c9b31ebd9def
“anishk74” <anishkumarkharwar@gmail.com>
2019-04-07 16:32:58 +0530

    added python script

---
+++ scrapLyrics.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+ 
+searchSong='https://www.google.com/search?q='
+s=input('Enter the song to find its lyrics: ')
+for i in s:
+	if i==' ':
+		searchSong+='+'	
+	else:
+		searchSong+=i
+searchSong+='+lyrics'
+
+
+options = webdriver.ChromeOptions();
+options.add_argument('headless');
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",options=options)
+
+driver.get(searchSong)
+
+driver.implicitly_wait(20)
+
+try:
+	expand=driver.find_element_by_css_selector("div[aria-expanded='false']")
+	driver.execute_script("document.querySelectorAll('div[aria-expanded=false]')[1].click()")
+	driver.execute_script("arguments[0].setAttribute('area-expanded','true')", expand)
+	driver.implicitly_wait(20)
+	e1=driver.find_element_by_class_name("bkWMgd")
+	lyrics=e1.get_attribute('textContent')
+	print(e1.text)
+except:
+	print('Couldn\'t find lyrics')
+
+finally:
+	driver.quit()
+	
+
+
+
