385dfeede6ecb8bcdcb950d39b2192174bd51710
“anishk74” <anishkumarkharwar@gmail.com>
2019-04-08 05:54:18 +0530

    Removed selenium added BeautifulSoup

--- scrapLyrics.py
+++ scrapLyrics.py
@@ -1,7 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
- 
+import requests
+from bs4 import BeautifulSoup, UnicodeDammit
+
+
 searchSong='https://www.google.com/search?q='
 s=input('Enter the song to find its lyrics: ')
 for i in s:
@@ -10,28 +10,21 @@ for i in s:
 	else:
 		searchSong+=i
 searchSong+='+lyrics'
+searchPage = requests.get(searchSong)
 
-
-options = webdriver.ChromeOptions();
-options.add_argument('headless');
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",options=options)
-
-driver.get(searchSong)
+html = BeautifulSoup(searchPage.text, "html.parser")
 
 try:
-	expand=driver.find_element_by_css_selector("div[aria-expanded='false']")
-	driver.execute_script("document.querySelectorAll('div[aria-expanded=false]')[1].click()")
-	driver.execute_script("arguments[0].setAttribute('area-expanded','true')", expand)
-
-	e1=driver.find_element_by_class_name("bkWMgd")
-
-	print(e1.text)
+	playMusicTag=html.find("a", {"class":"fl"})
+	playMusicLink=playMusicTag.get('href')
+	lyricsPage=requests.get(playMusicLink)
+	playMusicHTML = BeautifulSoup(lyricsPage.text, "html.parser")
+	for i in playMusicHTML.find_all("br"):
+		i.replace_with('\n')
+	lyricsDiv=playMusicHTML.find("div", {"class":"content-container lyrics"})
+	lyricsPara=lyricsDiv.findChildren("p")
+	for i in lyricsPara:
+		print(i.text)
 except:
-	print('Couldn\'t find lyrics')
-
-finally:
-	driver.quit()
-	
-
-
+	print("Couldn't get lyrics")
 
